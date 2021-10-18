ebe0f953c585e2961b7d2f5dc2561598e53a088a
SAPRIAL <s.prietoa@uniandes.edu.co>
2021-10-18 17:46:38 -0500

    Eliminación de un método

--- src/Actualizador.java
+++ src/Actualizador.java
@@ -10,33 +10,6 @@ public class Actualizador extends Thread{
         tabla = t;
     }
     
-    public int descubrirReemplazo() {    	
-    	int tamaño=tabla.marcos.length;
-    	int i=0;
-    	int opcionado = 0;
-    	int tope = 0;
-    	while (i<tamaño) {
-    		MarcoPagina marco=tabla.marcos[i];
-    		int clase = marco.darClase();
-    		if (clase==0) {
-    			opcionado=marco.pagCargada;
-    			break;
-    		}
-    		else if (clase==1) {
-    			opcionado=marco.pagCargada;
-    			tope=1;
-    		}
-    		else if (clase==2 && (tope==0 || tope>1)) {
-    			opcionado=marco.pagCargada;
-    			tope=2;
-    		}
-    		else if (clase==2 && (tope==0 || (tope>2))) {
-    			opcionado=marco.pagCargada;
-    			tope=3;
-    		}
-    	}
-    	return opcionado;
-    }
     
     public void actualizar() {
     	int tamaño=tabla.marcos.length;
